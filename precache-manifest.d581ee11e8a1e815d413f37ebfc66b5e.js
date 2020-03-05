014b144b9661a7dac3ef1a073c925eadb760798a
instacation <instacation@yahoo.com>
2020-03-05 23:10:31 +1100

    First site

---
+++ precache-manifest.d581ee11e8a1e815d413f37ebfc66b5e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed1bfb5c7ef5fe04982eedfeebc7b007",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b492e6b4bf5f4984ab07",
+    "url": "/static/css/main.7cd71a84.chunk.css"
+  },
+  {
+    "revision": "0e7f62f9a642b8e814bd",
+    "url": "/static/js/2.6a034ed0.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.6a034ed0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b492e6b4bf5f4984ab07",
+    "url": "/static/js/main.e4243870.chunk.js"
+  },
+  {
+    "revision": "5aaf58c5c93adf01ed4e",
+    "url": "/static/js/runtime-main.688d7c74.js"
+  },
+  {
+    "revision": "3c294099ac116fcfa9e2e06fd98fe42c",
+    "url": "/static/media/logo.3c294099.svg"
+  }
+]);
